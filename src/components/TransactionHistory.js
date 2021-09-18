338dd28ecd5198cbf2b0ada70acf456aab08e198
OlhaNester <olga_nv@ukr.net>
2021-09-18 18:21:40 +0300

    666

--- src/components/TransactionHistory.js
+++ src/components/TransactionHistory.js
@@ -1,25 +1,26 @@
 import React from 'react';
 
-const TransactionHistory = ({transactions}) => {
-    return (<table class="transaction-history">
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
-    </tr>
-        </thead>
-        <tbody>
-            {transactions.map(({ id, type, amount, currency }) => (
-                <tr key={ id}>
-                    <td>{type}</td>
-                    <td>{amount}</td>
-                    <td>{currency}</td>
-                </tr>))}
-  
-    
-  </tbody>
-</table>);
+const TransactionHistory = ({ items }) => {
+  return (
+    <table class="transaction-history">
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
+      <tbody>
+        {items.map(({ id, type, amount, currency }) => (
+          <tr key={id}>
+            <td>{type}</td>
+            <td>{amount}</td>
+            <td>{currency}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default TransactionHistory;
